59ea9d75a627ffd680ba2fe2652cc0e5bc0e9560
Lucía A. <u20211a452@upc.edu.pe>
2024-10-24 09:56:49 -0500

    Feature: Updated table and alerts information
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
    Co-Authored-By: Luis Alberto Trujillo Lopez <104176051+NotLT42@users.noreply.github.com>

--- MIAM/src/app/band/components/history/history.component.ts
+++ MIAM/src/app/band/components/history/history.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, inject, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 
 interface Alert {
   date: string;
@@ -9,6 +11,8 @@ interface Alert {
   description: string;
   caregiver: string;
   actions: string;
+  caregivernotes: string;
+  caregiverAttendedDate: string;
 }
 
 @Component({
@@ -16,14 +20,17 @@ interface Alert {
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
+
 export class HistoryComponent implements OnInit {
   displayedColumns: string[] = ['date', 'hour', 'patient', 'alertType', 'description', 'caregiver', 'actions'];
-  alerts = new MatTableDataSource<Alert>(); // Inicializamos con un tipo vacío
+  alerts = new MatTableDataSource<Alert>();
+  readonly dialog = inject(MatDialog);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor() {}
 
   ngOnInit(): void {
-    // Datos de ejemplo para mostrar en la tabla
     const exampleAlerts: Alert[] = [
       {
         date: '2024-09-24',
@@ -32,26 +39,72 @@ export class HistoryComponent implements OnInit {
         alertType: 'High risk of falling',
         description: 'Increased risk of falling',
         caregiver: 'Carolina Suarez',
-        actions: 'Monitored'
+        actions: 'Patient attended with no incidents',
+        caregivernotes: 'Patient is feeling dizzy but resting now',
+        caregiverAttendedDate: '2024-09-24 10:00 AM'
       },
       {
         date: '2024-09-24',
-        hour: '09:49 AM',
+        hour: '08:52 AM',
         patient: 'José Díaz',
         alertType: 'High temperature',
         description: 'Current temperature: 38.5°C',
         caregiver: 'Carolina Suarez',
-        actions: 'Monitored'
+        actions: 'Patient monitored and temperature decreased to 37.5°C',
+        caregivernotes: 'Patient is feeling better',
+        caregiverAttendedDate: '2024-09-24 09:10 AM'
       }
     ];
 
-    // Pasamos los datos de ejemplo a la tabla
     this.alerts.data = exampleAlerts;
   }
 
-  // Método para descargar el reporte (simulación)
+  ngAfterViewInit() {
+    this.alerts.paginator = this.paginator;
+  }
+
   downloadReport(): void {
     alert('Downloading report...');
-    // Aquí puedes agregar la lógica para descargar el reporte en formato deseado
+  }
+  
+  openDialog(alert: Alert) {
+    const dialogRef = this.dialog.open(DialogContent, {
+      data: alert 
+    });
+  
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-content',
+  templateUrl: 'dialog-content.html',
+  styleUrls: ['./history.component.css'],
+  standalone: true,
+  imports: [MatDialogModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogContent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Alert, // Recibiendo datos de la alerta
+    private dialogRef: MatDialogRef<DialogContent>
+  ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  calculateTimeSpent(): string {
+    const alertDateTime = new Date(`${this.data.date} ${this.data.hour}`);
+    const attendedDateTime = new Date(this.data.caregiverAttendedDate);
+    
+    const timeDiff = attendedDateTime.getTime() - alertDateTime.getTime(); 
+    
+    const minutes = Math.floor((timeDiff / 1000) / 60); 
+    
+    return `${minutes} minutes`; 
   }
 }
